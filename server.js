1ad9594e8e43ece06fa28855d6507ee2b53c1da3
Sia <ashkiani@yahoo.com>
2020-02-01 11:10:31 -0500

    Added static midleware to serve the js file. Also added a * route.

--- server.js
+++ server.js
@@ -13,13 +13,10 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 // Routes
 // =============================================================
-
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
 
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public", "notes.html"));
@@ -29,6 +26,10 @@ app.get("/api/notes", function (req, res) {
     let jsonContent = getJSONData();
     return res.json(jsonContent);
     // res.sendFile(path.join(__dirname, "db", "db.json"));
+});
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Create New Note - takes in JSON input
